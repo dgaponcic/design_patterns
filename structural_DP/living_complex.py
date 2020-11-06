b1d61e17b7e16aff4aac3adf881de04149219081
diana <gaponcic.d@gmail.com>
2020-11-06 17:22:25 +0200

    add housing entities

---
+++ structural_DP/living_complex.py
@@ -0,0 +1,57 @@
+from living_space import Apartament, Townhouse
+from composite import Composite 
+
+class LivingComplex(Composite):
+  def add_parking_lot(self, parking_lot):
+    self.outside.add_parking_lot(parking_lot)
+  
+  def add_playground(self, playground):
+    self.outside.add_playground(playground)
+
+  def use_service(self):
+    self.service.provide_service()
+
+
+
+
+class ApartamentComplex(LivingComplex):
+  def __init__(self, entrances, floors, apartaments_per_floor, service, outside, basement):
+    self.entrances = entrances
+    self.floors = floors
+    self.apartaments_per_floor = apartaments_per_floor
+    self.apartaments = []
+    self.service = service
+    self.outside = outside
+    self.basement = basement
+    self._children = set([self.outside, self.basement])
+
+  def add_apartament(self, apartament):
+    max_nb_apartaments = self.entrances * self.apartaments_per_floor * self.floors
+
+    if max_nb_apartaments == len(self.apartaments):
+      raise "Maximum number of apartamens"
+    else:
+      self.apartaments.append(apartament)
+      self._children.add(apartament)
+
+
+
+
+
+class TownhouseComplex(LivingComplex):
+  def __init__(self, floors, entrances, service):
+    self.houses = []
+    self.floors = floors
+    self.entrances = entrances
+    self.service = service
+    self.outside = outside
+    self.basement = basement
+    self._children = set([self.outside, self.basement])
+
+  def add_house(self, house):
+    if self.entrances == len(self.houses):
+      raise "Maximum number of houses"
+    else:
+      self.houses.append(house)
+      self._children.add(house)
+
---
+++ structural_DP/living_space.py
@@ -0,0 +1,21 @@
+class LivingSpace:
+  def get_temperature(self, season):
+    seasons = {"autumn": "chilly", "winter": "normal", "spring": "norm", "summer": "hot"}
+    return seasons[season]
+
+class Apartament(LivingSpace):
+  def __init__(self, rooms, apartament_nb):
+    self.rooms = rooms
+    self.apartament_nb = apartament_nb
+    self.autonomous_heating = False
+    self.air_conditioner = False
+  
+  def get_area(self):
+    return self.rooms * 30
+
+class Townhouse(LivingSpace):
+  def __init__(self, rooms, house_nb):
+    self.rooms = rooms
+    self.house_nb = house_nb
+    self.autonomous_heating = False
+    self.air_conditioner = False
