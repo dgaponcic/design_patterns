fae65e1ddc64c39e15935e9ab9f0475c15cfaf1d
diana <gaponcic.d@gmail.com>
2020-11-06 17:23:07 +0200

    add lateral housing entities

---
+++ structural_DP/basement.py
@@ -0,0 +1,6 @@
+class Basement:
+  def __init__(self, area):
+    self.area = area
+
+  def get_area(self):
+    return self.area
---
+++ structural_DP/outside.py
@@ -0,0 +1,35 @@
+from functools import reduce
+from composite import Composite
+
+class Outside(Composite):
+  def __init__(self):
+    self.playgrounds = []
+    self.parking_lots = []
+    self._children = set()
+
+  def add_parking_lot(self, parking_lot):
+    self.parking_lots.append(parking_lot)
+    self._children.add(parking_lot)
+
+  def add_playground(self, playground):
+    self.playgrounds.append(playground)
+    self._children.add(playground)
+    
+
+
+class ParkingLots:
+  def __init__(self, places, area):
+    self.area = area
+    self.places = places
+
+  def get_area(self):
+    return self.area
+
+
+
+class Playgrounds:
+  def __init__(self, area):
+    self.area = area
+
+  def get_area(self):
+    return self.area
